77f0c5812c3cd9f93535b514b01ff5e4d7f79fe1
marinho14 <marinosebastian5@gmail.com>
2024-04-03 19:46:38 -0500

    add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MLops_P2
+
+
+docker exec -it mlops_p2-mysql-1 bash   
+mysql -u airflow -p airflow   
--- test.py
+++ test.py
@@ -7,9 +7,25 @@ headers = {'accept': 'application/json'}
 
 response = requests.get(url, params=params, headers=headers)
 
+COLUMN_NAMES = [
+    'Elevation', 
+    'Aspect', 
+    'Slope', 
+    'Horizontal_Distance_To_Hydrology',
+    'Vertical_Distance_To_Hydrology',
+    'Horizontal_Distance_To_Roadways',
+    'Hillshade_9am',
+    'Hillshade_Noon',
+    'Hillshade_3pm',
+    'Horizontal_Distance_To_Fire_Points',
+    'Wilderness_Area',
+    'Soil_Type',
+    'Cover_Type']
+
 if response.status_code == 200:
     json_data = response.json()
     df = pd.DataFrame.from_dict(json_data["data"])
+    df.columns = COLUMN_NAMES
     print(df)
 else:
     print("Error al realizar la solicitud:", response.status_code)
